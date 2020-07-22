d17cf57a2b37b98d83964196aad710f6e171c6af
dawsond-smu <62529301+dawsond-smu@users.noreply.github.com>
2020-07-21 23:34:52 -0400

    Updating the files

---
+++ Case Study 1 (Beers & Breweries)/App.R
@@ -0,0 +1,31 @@
+#App to explore Case Study 1
+library(data.table)
+library(DT)
+library(forcats)
+library(ggplot2)
+library(haven)
+library(Hmisc)
+library(plotly)
+library(scales)
+library(shiny)
+library(stringi)
+##1. Let the user read in the data (the Beer Data.) 
+
+##2. Let the user make a histogram of the IBU
+
+##3. Let the user make a histogram of the ABV  (separate histogram)
+
+##4. Low the user switch back and forth between histograms and boxplots to represent this information.  You could do this with a radio button that switches the plot from histogram to boxplot … the implementation however is up to you.  
+
+##5. Allow the user to filter the histograms / boxplots above by state. 
+
+##6. In addition to the histograms, add a scatter plot of IBU v. ABV
+
+##7. Add the ability to add or take off the simple linear regression line. 
+
+##8. Allow the user to filter the data on the scatter plot by state.
+
+##9. Add at least one additional plot from your analysis and make sure to clearly label and/or describe the information it is conveying.  
+
+
+
